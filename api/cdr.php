7be0947d381694773bbcc71b4b4b331aca633fb3
Alex Shubin <Alex.Shubin@gmail.com>
2025-06-02 20:58:18 +0300

    deduplicate_unique_id = 0

--- api/cdr.php
+++ api/cdr.php
@@ -37,6 +37,11 @@ $minDuration = isset($_GET['min_duration']) ? (int)$_GET['min_duration'] : null;
 $answered = isset($_GET['answered']) ? (int)$_GET['answered'] : null;
 $fieldset = $_GET['fieldset'] ?? ''; // all - для отладки
 
+$deduplicateUniqueId = true;
+if (isset($_GET['deduplicate_unique_id'])) {
+    $deduplicateUniqueId = $_GET['deduplicate_unique_id'] === '1';
+}
+
 $offset = ($page - 1) * $perPage;
 
 function extractExtension($channel)
@@ -149,7 +154,7 @@ $grouped = [];
 foreach ($results as $row) {
     // Пропускаем дубликаты по uniqueid
     $uid = $row['uniqueid'];
-    if (isset($seen[$uid])) {
+    if (isset($seen[$uid]) && $deduplicateUniqueId) {
         continue;
     }
     $seen[$uid] = true;
